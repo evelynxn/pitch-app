f06b0efddc976c3871694c909c15501eec640632
Evelyn Nguyen <evelyn@Evelyns-Air-3.cogeco.local>
2025-10-01 23:01:20 -0400

    get user media audio, and loaded ml5 model

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, useRef } from 'react';
+import './App.css';
+import ml5 from "ml5";
+const modelURL = "/models"
+
 
 function App() {
+
+  const[modelLoaded, setModelLoaded] = useState(false);
+
+  let pitch;
+  const audioContext = useRef();
+  const micInput = useRef();
+  audioContext.current = new AudioContext ;
+
+  useEffect(()=>{
+    (async()=>{
+      const micInput = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false
+      });
+      pitch.current = ml5.pitchDetection(
+        modelURL,
+        audioContext.current,
+        micInput,
+        ()=> setModelLoaded(true)
+      );
+    })();
+  },[]);
+
+ 
+
+
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main style={{ fontFamily: "system-ui", padding: 24 }}>
+      <h1>My First React App 🚀</h1>
+      <p>Hello world! This is React + Vite.</p>
+      <button onClick={() => setCount(c => c + 1)}>
+        You clicked {count} times
+      </button>
+    </main>
+  );
 }
+
 
 export default App
